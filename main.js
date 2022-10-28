d8d0e275ec65428325e790908ced383b9214b93a
Diego Beltrán <diegoandreseffie@hotmail.com>
2022-10-27 19:02:46 -0500

    Adicion del carrito de compras

--- main.js
+++ main.js
@@ -1,16 +1,44 @@
 let menuEmail = document.querySelector('.navbar-email');
 let desktopMenu = document.querySelector('.desktop-menu');
 let menuHamIcon = document.querySelector('.menu');
-let mobileMenu = document.querySelector('.mobile-menu')
+let mobileMenu = document.querySelector('.mobile-menu');
+let menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+let aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
     
 }
 
 function toggleMobileMenu() {
+
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+
 }
